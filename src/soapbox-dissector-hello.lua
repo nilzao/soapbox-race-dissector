8ac4ca03fb86581cb05d32512332d79a5b74f54c
Nilzao <nilzao@gmail.com>
2017-03-05 19:28:32 -0400

    more hello header

--- src/soapbox-dissector-hello.lua
+++ src/soapbox-dissector-hello.lua
@@ -1,4 +1,9 @@
 function setHelloFields(buf, pkt, subtree)
+  local cli_cli_type = detectDirection(pkt)
+  if(cli_cli_type == 'srv->cli') then
+    setCountField(buf, subtree, 2)
+    setTimeField(buf,subtree,4)
+  end
   pkt.cols.protocol = 'SB-HELLO'
-  pkt.cols.info = 'Hello Protocol'
+  pkt.cols.info = 'Hello Protocol ['..cli_cli_type..']'
 end
