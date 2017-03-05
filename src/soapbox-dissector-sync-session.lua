1c0d02f1597462300d3b914de8759cff0c1f0430
Nilzao <nilzao@gmail.com>
2017-03-05 19:18:36 -0400

    more debug info

--- src/soapbox-dissector-sync-session.lua
+++ src/soapbox-dissector-sync-session.lua
@@ -1,8 +1,9 @@
 function setSyncSessionFields(buf, pkt, subtree)
+  local cli_cli_type = detectDirection(pkt)
   if(cli_cli_type == 'srv->cli') then
     setCountField(buf, subtree, 2)
     setTimeField(buf,subtree,4)
   end
   pkt.cols.protocol = 'SB-SYNC-S'
-  pkt.cols.info = 'Sync Session Protocol'
+  pkt.cols.info = 'Sync Session Protocol ['..cli_cli_type..']'
 end
--- src/soapbox-dissector-sync.lua
+++ src/soapbox-dissector-sync.lua
@@ -1,8 +1,9 @@
 function setSyncFields(buf, pkt, subtree)
+  local cli_cli_type = detectDirection(pkt)
   if(cli_cli_type == 'srv->cli') then
     setCountField(buf, subtree, 2)
     setTimeField(buf,subtree,4)
   end
   pkt.cols.protocol = 'SB-SYNC'
-  pkt.cols.info = 'Sync Protocol'
+  pkt.cols.info = 'Sync Protocol ['..cli_cli_type..']'
 end
