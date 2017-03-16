cd0321a44593e7c20ef1b4e01c69d3905e7653a7
Nilzao <nilzao@gmail.com>
2017-03-16 17:01:28 -0400

    fields order

--- src/soapbox-dissector-player.lua
+++ src/soapbox-dissector-player.lua
@@ -6,12 +6,12 @@ function setPlayerFields(buf, pkt, subtree)
   setBeforeHandShakeField(buf,pkt,subtree)
   setStaticFFp2pField(buf,pkt,subtree)
   local unknownEnum = setUnknownP2PEnum(8, buf, pkt, subtree)
-  local debug = unknownEnum
+  local packetSize = 0
+  setSubPacketSize(9, buf, pkt, subtree)
+  --setUnknownP2PSequence(buf, pkt, subtree, 10)
   if(unknownEnum == 2) then
     setIdFields(buf,pkt,subtree)
   end
-  local packetSize = 0
-  setSubPacketSize(9, buf, pkt, subtree)
   packetSize = setSubPacket(9, buf, pkt, subtree)
   packetSize = packetSize + 10
   unknownEnum = setUnknownP2PEnum(packetSize, buf, pkt, subtree)
@@ -23,5 +23,5 @@ function setPlayerFields(buf, pkt, subtree)
   setPacketField(buf,pkt,subtree)
 
   pkt.cols.protocol = 'SB-PLAYER'
-  pkt.cols.info = 'Player Protocol ['..cli_cli_type..']'..debug
+  pkt.cols.info = 'Player Protocol ['..cli_cli_type..']'
 end
