5bfb373c0053ab0eefbb5472cc7a2e8cead1be5c
Nilzao <nilzao@gmail.com>
2018-01-26 11:18:23 -0300

    zx position (with Y higher than 2515)

--- src/modules/soapbox/dissector-statepos.lua
+++ src/modules/soapbox/dissector-statepos.lua
@@ -3,7 +3,9 @@ function setStatePosFields(start, buf, pkt, subtree)
   subtree:add(f_sb_flying, buf(start + 4, 1))
   local notflying = buf:range(start + 4, 1):bitfield(4)
   if(notflying == 1) then
-    --parse yzx
+    --parse (works only if /myposition Y > 2515
     subtree:add(f_sb_y, buf(start + 5, 3))
+    subtree:add(f_sb_z, buf(start + 7, 3))
+    subtree:add(f_sb_x, buf(start + 9, 3))
   end
 end
