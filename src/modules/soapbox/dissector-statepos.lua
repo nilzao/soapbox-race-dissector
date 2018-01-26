29c21e4e64966626db47f008212dc27be0b66e7a
Nilzao <nilzao@gmail.com>
2018-01-26 14:27:37 -0300

    trying to bitwise

--- src/modules/soapbox/dissector-statepos.lua
+++ src/modules/soapbox/dissector-statepos.lua
@@ -3,9 +3,15 @@ function setStatePosFields(start, buf, pkt, subtree)
   subtree:add(f_sb_flying, buf(start + 4, 1))
   local notflying = buf:range(start + 4, 1):bitfield(4)
   if(notflying == 1) then
-    --parse (works only if /myposition Y > 2515
     subtree:add(f_sb_y, buf(start + 5, 3))
-    subtree:add(f_sb_z, buf(start + 7, 3))
-    subtree:add(f_sb_x, buf(start + 9, 3))
+    local yHeader = buf:range(start + 5, 3):bitfield(12,10)
+    if(yHeader > 970) then
+      subtree:add(f_sb_z, buf(start + 7, 3))
+      subtree:add(f_sb_x, buf(start + 9, 3))
+    else
+      subtree:add(f_sb_z2, buf(start + 7, 3))
+      subtree:add(f_sb_x2, buf(start + 9, 3))
+    end
+
   end
 end
